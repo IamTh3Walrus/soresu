8ce1e38c8195105060022ee9d487ba449d01552a
Stephen Doe <Stephen Doe>
2017-04-24 13:07:01 -0400

    fixes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
-//database: 'mongodb://localhost:27017/soresu', //development
 module.exports = {
-  database: 'mongodb://StephenDoe:MoffTarkin@ds161960.mlab.com:61960/soresu', //production
+  database: 'mongodb://localhost:27017/soresu', //development
+  //database: 'mongodb://StephenDoe:MoffTarkin@ds161960.mlab.com:61960/soresu', //production
   secret: 'yoursecret'
 }
